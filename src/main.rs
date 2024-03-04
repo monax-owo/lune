44cb7ba11ad0b038d9be6a72a50821b680691eea
zprl <zprl.dev@gmail.com>
2024-03-05 06:55:51 +0900

    メンタルが終わりましたので。 今日はResult型とOption<T>について少し学びました。 unwrapは値がNoneやErrだった際にパニックさせるメソッドです。 エラーハンドリングには使わないようにしましょう。

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
-}
+use crossterm::{
+    event::{self, KeyCode, KeyEventKind},
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+    ExecutableCommand,
+};
+use ratatui::{
+    prelude::{CrosstermBackend, Stylize, Terminal},
+    widgets::Paragraph,
+};
+use std::io::{stdout, Result};
